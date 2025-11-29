3ceba50570472cf0ec14f317e39a138aada8c757
Devansh Sood <sooddevansh143.gmail.com>
2025-11-29 18:36:28 +0530

    final output

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let wraper=document.querySelector('.video-wraper');
 
-
+let video=document.querySelector('video');
 
 wraper.addEventListener('mousemove',(e)=>{
 
@@ -12,11 +12,62 @@ wraper.style.setProperty('--y',e.offsetY+'px');
 
 
 
+
+wraper.addEventListener("mouseenter", () =>{
+
+video.muted=false; 
+
+ video.play()});
+
+
+
+
 wraper.addEventListener("mouseleave", () => {
   video.pause();
   wraper.style.setProperty("--x", "-9999px");
   wraper.style.setProperty("--y", "-9999px");
 });
 
-wraper.addEventListener("mouseenter", () => video.play());
-// wrapper.addEventListener("mouseleave", () => video.pause());
+
+
+// now adding the matrix effect to the text 
+
+
+
+const h1= document.querySelector('h1');
+const character="さねみよりほくたせきえま";
+
+const text=h1.innerText;
+let iteration=0;
+let interval=null;
+
+function random(){
+
+const str= text.split("").map((char,index)=>{
+
+  if(index<iteration){
+    return char;
+  }
+
+  return character.split("")[Math.floor(Math.random()*13)]
+}).join("");
+
+
+h1.innerText=str;
+iteration +=0.1;
+
+ if(iteration>=text.length){
+        clearInterval(intervall);
+    }
+
+
+
+}// random function ends here
+
+
+h1.addEventListener('mouseenter',function(){
+  iteration=0;
+  clearInterval(interval);
+interval=setInterval(random,30);
+
+})
